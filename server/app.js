b937a250f51b517dcea31171b0b8441f638b14a3
Patty Long <pattygxlong@gmail.com>
2022-11-29 19:21:03 -0800

    Complete Adding Post Routes

--- server/app.js
+++ server/app.js
@@ -16,66 +16,123 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 
-app.get('/', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/create',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
-
-app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
-
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
-        throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+app.get('/links',
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
       });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
-    });
-});
+  });
+
+app.post('/links',
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
+      // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
+
+    return models.Links.get({ url })
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
+        throw link;
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
+      });
+  });
 
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+
+app.get('/signup', (req, res, next) => {
+  res.render('signup');
+});
+
+app.post('/signup', (req, res, next) => {
+  // console.log('post signup');
+  // console.log(req.body);
+  // console.log(req.body.username);
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  models.Users.get({ username })
+    .then((data) => {
+      if (data) {
+        // send them to the login
+        res.redirect('/login');
+      } else {
+        //create the user
+        models.Users.create({ username, password })
+          .then(user => {
+            res.status(201).send(user);
+            //return user;
+          })
+          .error(error => {
+            console.log('error');
+            res.status(500).send(error);
+          });
+      }
+    });
+});
+
+
+app.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+
+  models.Users.get({ username }).then((data) => {
+    // console.log(data, 'databody');
+    // console.log(data.password, 'pass');
+    if (models.Users.compare(password, data.password, data.salt)) {
+      res.redirect('/');
+    } else {
+      res.redirect('/login');
+    }
+
+  });
+
+
+
+});
 
 
 
